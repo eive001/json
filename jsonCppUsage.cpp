d1bcda71af2573e67c70f93a5a1dd45c8904272e
eive001 <48793440@qq.com>
2022-08-02 09:16:04 +0000

    first commit

---
+++ jsonCppUsage.cpp
@@ -0,0 +1,36 @@
+// jsonCppUsage.cpp
+#include <iostream>
+#include "json/json.h"
+ 
+using namespace std;
+ 
+int main()
+{
+    // 解析json串
+    string message = "{ \"data\" : { \"username\" : \"test\" }, \"type\" : 6 }";
+    Json::Reader reader(Json::Features::strictMode());
+    Json::Value parseData;
+    if (reader.parse(message.c_str(), parseData))
+    {
+        int type = parseData.get("type", 0).asInt();
+        Json::Value frameData = parseData.get("data", 0);
+        string username = frameData.get("username", 0).asString();
+        printf("type: %d, username: %s\n", type, username.c_str());
+    }
+ 
+    // 生成json串
+    Json::Value data;
+    Json::Value frame;
+    Json::FastWriter fastWriter;
+ 
+    frame["type"] = 66;
+    data["username"] = "test";
+    frame["data"] = data;
+ 
+    string result = fastWriter.write(frame);
+    printf("result: %s \n", result.c_str());
+ 
+    return 0;
+}
+
+
